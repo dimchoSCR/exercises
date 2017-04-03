394c049d84db4acf4e8c77f00b2d8fb70832a0f6
dimchoSCR <dimchokarpachev@gmail.com>
2017-04-03 19:49:02 +0300

    Solutions

--- src/ex2/Architect.scala
+++ src/ex2/Architect.scala
@@ -16,10 +16,36 @@ class Architect {
    *     val o: Option[Int] = Some(6)
    *     val n: Int = o.get
    */
-  def max(xs: List[Int]): Option[Int] = ???
+  def max(xs: List[Int]): Option[Int] = {
+    def iter(xs: List[Int],curMax:Int): Option[Int] = {
+      if(xs.isEmpty) Some(curMax) 
+      else {
+        iter(xs.tail,if (xs.head > curMax) xs.head else curMax);
+      }
+    }
+    
+    if(xs.isEmpty) None else iter(xs.tail, xs.head);
+    
+  }
 
   // Determines the type of given triangle: "rectangular", "equilateral", "isosceles", "random"
-  def triangleType(t: Triangle): String = ???
+  def triangleType(t: Triangle): String =  {
+    if(!isTriangle(t)) return "not";
+    
+    if (t.a != t.b && t.b != t.c && t.a!=t.c){
+      val aSquare = t.a*t.a;
+      val bSquare = t.b*t.b;
+      val cSquare = t.c*t.c;
+      
+      // Check for rectangular triangle
+      if((aSquare + bSquare == cSquare)
+          || (aSquare + cSquare == bSquare) 
+          || (bSquare + cSquare == aSquare)) "rectangular" else "random";
+               
+    }else if(t.a == t.b && t.b == t.c) "equilateral";
+     else "isosceles";
+    
+  }
 
   /*
    * Calculates the area of the provided shape, by using these formulae:
@@ -28,10 +54,24 @@ class Architect {
    *  - Rectangle: a * b, where a and b are both sides
    *  - Trapezoid: (a + b) * h / 2, where a and b are the parallel sides and h is the height between them
    *  - Cube: always return -1
-   *  
+   * 
    *  Hint: for triangles use the max function
    */
-  def area(s: Shape): Double = ???
+  def area(s: Shape): Double = s match{
+    case t:Triangle => triangleType(t) match{
+      case "rectangular" => max(List(t.a,t.b,t.c)).get match{
+        case t.a => (t.b * t.c) / 2;
+        case t.b => (t.a * t.c) / 2;
+        case t.c => (t.a * t.b) / 2;
+      }
+      // Returns -2 if not a triangle
+      case "not" => -2;
+      case _ => (max(List(t.a,t.b,t.c)).get * t.h) .toDouble / 2;
+    }
+    case Rectangle(a,b) => a*b;
+    case Trapezoid(a,b,h) => (a + b).toDouble * h / 2;
+    case Cube() =>  -1;
+  }
 
   /*
    *  Returns the number of rectangular triangles in given list of shapes
@@ -39,7 +79,17 @@ class Architect {
    *  Hint: use the triangleType function
    */
   def findRectangulars(shapes: List[Shape]): Int = {
-    def iter(shapes: List[Shape], n: Int): Int = ???
-    iter(???, ???)
+    def iter(shapes: List[Shape], n: Int): Int = {
+      if(shapes.isEmpty) n;
+      else iter(shapes.tail,shapes.head match{
+        case t:Triangle => if(triangleType(t).equals("rectangular")) n+1 else n;
+        case s:Shape => n; 
+      });       
+    }
+    iter(shapes, 0);
+  }
+  
+  def isTriangle(t:Triangle)={
+    if(t.a + t.b > t.c && t.b + t.c > t.a && t.a + t.c > t.b) true else false;
   }
 }
